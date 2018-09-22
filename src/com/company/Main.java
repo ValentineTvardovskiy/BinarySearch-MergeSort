66bcecb9b444b0add5545c95697ebf86a87e15ea
Valentine Tvardovskiy <valentintw1999@gmail.com>
2018-09-23 01:46:03 +0300

    version-1

---
+++ src/com/company/Main.java
@@ -0,0 +1,78 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] a = { 5,6,9,10,15,1,3,4 };
+        int[] b = { 5,5,1,3,4,5,5,5,5 };
+        int[] c = { 1,2,3,3,4,4,4,4,4 };
+        //5,6,9,10,15,1,3,4
+        //5,5,1,3,4,5,5,5,5
+        //1,2,3,3,3,3,3,3,3
+
+        System.out.println(search(a, 5));
+        System.out.println(search(a, 1));
+        System.out.println(search(a, 4));
+        System.out.println(search(a, 3));
+        System.out.println(search(a, 8));
+        System.out.println();
+        System.out.println(search(b, 5));
+        System.out.println(search(b, 3));
+        System.out.println();
+        System.out.println(search(c, 3));
+        System.out.println(search(c, 2));
+
+
+    }
+
+    public static int search(int a[], int x) {
+        return search(a, 0, a.length - 1, x);
+    }
+
+
+    public static int search(int a[], int left, int right, int x) {
+        int mid = (left + right) / 2;
+        
+        if (x == a[0]){
+            return 0;
+        }
+        if (x == a[mid]) {
+            return (a[mid] == a[mid - 1]) ? search(a, left, mid - 1, x) : mid;
+        }
+        if (right < left) {
+            return -1;
+        }
+
+        if (a[left] < a[mid]) {
+            return (x >= a[left] && x < a[mid]) ?
+                    search(a, left, mid - 1, x) : search(a, mid + 1, right, x);
+        } else if (a[mid] < a[left]) {
+            return (x > a[mid] && x <= a[right]) ?
+                    search(a, mid + 1, right, x) : search(a, left, mid - 1, x);
+        } else if (a[left] == a[mid]) {
+            return  (a[mid] != a[right]) ?
+                    search(a, mid + 1, right, x) : search(a, left, mid - 1, x);
+        }
+        return -1;
+    }
+/*
+    private static int binarySearch0(int[] a, int fromIndex, int toIndex, int key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int midVal = a[mid];
+
+            if (midVal < key)
+                low = mid + 1;
+            else if (midVal > key)
+                high = mid - 1;
+            else
+                return mid; // key found
+        }
+        return -(low + 1);  // key not found.
+    }
+*/
+}
